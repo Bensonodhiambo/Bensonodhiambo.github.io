f1c4a39bc06b5031666b72430afc1eeaa8191d80
Oscar Muya <106892939+oscarmuya@users.noreply.github.com>
2024-09-21 22:00:28 +0300

    flask changes 1. html files 2. css files 3. main flask python file.

--- maze_api.py
+++ maze_api.py
@@ -1,19 +1,31 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, render_template
 import subprocess
 
 app = Flask(__name__)
 
+
 def get_maze_from_c():
     # Running the C program to generate the maze
-    result = subprocess.run(['./maze'], stdout=subprocess.PIPE)
-    maze_output = result.stdout.decode('utf-8').splitlines()
+    result = subprocess.run(["./maze.exe"], stdout=subprocess.PIPE)
+    maze_output = result.stdout.decode("utf-8").splitlines()
     return maze_output
 
-@app.route('/api/maze', methods=['GET'])
+
+@app.route("/api/maze", methods=["GET"])
 def get_maze():
     maze = get_maze_from_c()
     return jsonify(maze)
 
-if __name__ == '__main__':
-    app.run(debug=True)
 
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
